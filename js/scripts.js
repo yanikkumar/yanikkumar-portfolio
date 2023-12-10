0062972fe1c3368905129b4faf17d4d153377d33
Yanik Kumar <believemaster@gmail.com>
2023-12-10 19:10:24 +0530

    Some changes in feature image

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+$(".nav-item .nav-link").on("click", function(){
+    $(".nav-item").find(".active").removeClass("active");
+    $(this).addClass("active");
+ });
+
+ $('#linktreeModal iframe').attr('src', $('#linktreeBtn').attr('href') );
